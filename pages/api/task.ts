ccfbd543b2a3846f757d2e584a316f72b7a0bf40
luisfelipe2104 <100540998+luisfelipe2104@users.noreply.github.com>
2023-06-30 16:56:09 -0300

    getting tasks

--- pages/api/task.ts
+++ pages/api/task.ts
@@ -1,38 +1,52 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 type Data = {
-  msg: string
-}
+  msg: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-    try {
-        console.log(req.method);
-        
-        const { taskName, taskDescription, taskGoal, user_id } = req.body
+  try {
+    console.log(req.method);
 
-        if (!taskName || !taskGoal || !user_id) { 
-          return res.status(406).json({ msg: `data can't be empty!` })
+    switch (req.method) {
+// ---------------------------------------------------------------------------
+      case "POST":
+        const { taskName, taskDescription, taskGoal, user_id } = req.body;
+
+        if (!taskName || !taskGoal || !user_id) {
+          return res.status(406).json({ msg: `data can't be empty!` });
         }
 
         await prisma.$connect();
         const data = { user_id, taskName, taskDescription, taskGoal };
-    
+
         const result = await prisma.task.create({
-          data: data
+          data: data,
         });
+
+        return res.status(200).json({ msg: "task created!" });
         
-        return res.status(200).json({ msg: 'task created!' })
-      } catch (err: any) {
-        console.log(err);
-        res.status(400).json({ msg: err.message })
-      } finally {
-        async () => {
-          await prisma.$disconnect();
-        };
-      }
+// ------------------------------------------------------------------------
+      case 'GET':
+        const { userId } : any = req.query
+        const HabitData: any = await prisma.task.findMany({
+          where: { user_id: userId }
+        })
+        return res.status(200).json(HabitData)
+
+// ------------------------------------------------------------------------
+    }
+  } catch (err: any) {
+    console.log(err);
+    res.status(400).json({ msg: err.message });
+  } finally {
+    async () => {
+      await prisma.$disconnect();
+    };
+  }
 }
